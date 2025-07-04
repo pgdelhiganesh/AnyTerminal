2e01ee346ec54e7fd297be9320d23cd63b9ab8ef
DELHIGANESH GOVINDASAMI <delhiganesh.g@winomech.com>
2025-07-04 10:14:21 +0530

    ReleaseDate changed

--- AnyTerminalApp.Net472/AppInfo.cs
+++ AnyTerminalApp.Net472/AppInfo.cs
@@ -4,6 +4,6 @@
   {
     public static string Name = "AnyTerminal";
     public static string ReleaseVersion = "0.0.1";
-    public static string ReleaseDate = "2025-07-02";
+    public static string ReleaseDate = "2025-04-07";
   }
 }
